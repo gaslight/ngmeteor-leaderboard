49fda00d534170d11f8153151613c2b79751b7fc
Chris Nelson <chris@gaslightsoftware.com>
2014-08-01 13:56:58 -0400

    github auth is working

--- client/leader_board_controller.js
+++ client/leader_board_controller.js
@@ -1,8 +1,10 @@
-angular.module("app").controller("LeaderBoardController", function($scope, $collection) {
+angular.module("app").controller("LeaderBoardController", ["$scope", "$collection", "$user", function($scope, $collection, $user) {
 
   $scope.selected = {};
 
   $collection(Players).bind($scope, 'players');
+
+  $user.bind($scope, "currentUser")
 
   $scope.isSelected = function(player) {
     return this.selected.player == player;
@@ -16,4 +18,4 @@ angular.module("app").controller("LeaderBoardController", function($scope, $coll
     this.selected.player.score = this.selected.player.score + 5;
     this.players.save(this.selected.player);
   }
-});
+}]);
